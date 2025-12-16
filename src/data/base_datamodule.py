fa55dfb055da148c0d6cc2fb98495d81f79f9134
Thijs van der Plas <vdplasthijs@gmail.com>
2025-12-16 18:20:51 +0100

    Change splits to id based rather than row-index based

--- src/data/base_datamodule.py
+++ src/data/base_datamodule.py
@@ -4,7 +4,8 @@ import os
 import torch
 from torch.utils.data import DataLoader, random_split
 from lightning import LightningDataModule
-
+import pandas as pd
+import numpy as np
 from src.data.base_dataset import BaseDataset
 from src.models.components.collate_fns import collate_fn
 from src.utils.errors import IllegalArgumentCombination
@@ -19,7 +20,9 @@ class BaseDataModule(LightningDataModule):
             num_workers: int = 0,
             pin_memory: bool = False,
             split_mode: str = 'random',
-            filepath_split_indices: str = None
+            save_split: bool = False,
+            filepath_split_indices_load: str | None = None,
+            filepath_split_indices_save: str | None = None
     ) -> None:
         super().__init__()
         self.save_hyperparameters(logger=False)
@@ -43,7 +46,7 @@ class BaseDataModule(LightningDataModule):
                 )
             self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
 
-    def setup(self, stage:str = 'fit') -> None:
+    def setup(self, stage: str = 'fit') -> None:
         self.setup_batch_size_per_device()
 
         if self.hparams.split_mode == "random":
@@ -55,26 +58,44 @@ class BaseDataModule(LightningDataModule):
             print(f'Dataset was randomly split with proportions: {self.hparams.train_val_test_split}')
         elif self.hparams.split_mode == "from_file":
             assert self.hparams.filepath_split_indices is not None, IllegalArgumentCombination(f"filepath_split_indices must be provided when split_mode is 'from_file'")
-
-            split_indices = self.load_split_indices(self.hparams.filepath_split_indices)
+            self.hparams.save_split = False  ## don't save split when loading from file
+            split_indices = self.load_split_indices(self.hparams.filepath_split_indices_load)
             train_indices = split_indices['train_indices']
             val_indices = split_indices['val_indices']
             test_indices = split_indices.get('test_indices', None)
             
+            if type(train_indices) != pd.Series: raise NotImplementedError('Expected a pd series of ids for data splits.')
+            if type(val_indices) != pd.Series: raise NotImplementedError('Expected a pd series of ids for data splits.')
+            if test_indices is not None and type(test_indices) != pd.Series: raise NotImplementedError('Expected a pd series of ids for data splits.')
+
+            train_indices = np.where(self.dataset.df['id'].isin(train_indices))[0]
             self.data_train = torch.utils.data.Subset(self.dataset, train_indices)
             self.data_train.dataset.mode = 'train'
+            val_indices = np.where(self.dataset.df['id'].isin(val_indices))[0]
             self.data_val = torch.utils.data.Subset(self.dataset, val_indices)
             self.data_val.dataset.mode = 'val'
 
             if test_indices is not None:
+                test_indices = np.where(self.dataset.df['id'].isin(test_indices))[0]
                 self.data_test = torch.utils.data.Subset(self.dataset, test_indices)
                 self.data_test.dataset.mode = 'test'
             else:
                 self.data_test = None
-            print(f'Dataset was split using indices from file: {self.hparams.filepath_split_indices}')
+            print(f'Dataset was split using indices from file: {self.hparams.filepath_split_indices_load}')
         else:
             raise NotImplementedError(f'{self.hparams.train_val_test_split} split mode not implemented.')
 
+        if self.hparams.save_split:
+            assert self.hparams.filepath_split_indices_save is not None, "filepath_split_indices_save must be provided when saving a new data split."
+            assert os.path.exists(os.path.dirname(self.hparams.filepath_split_indices_save)), f"Directory to save split indices does not exist: {os.path.dirname(self.hparams.filepath_split_indices_save)}"
+            split_indices = {
+                'train_indices': self.data_train.get('id'),
+                'val_indices': self.data_val.get('id'),
+                'test_indices': self.data_test.get('id')
+            }
+            timestamp = du.create_timestamp()
+            torch.save(split_indices, os.path.join(self.hparams.filepath_split_indices_save, f'split_indices_{self.dataset_name}_{timestamp}.pth'))
+            print(f'Saved split indices to split_indices_{timestamp}.pth')
 
     def load_split_indices(self, filepath: str = None) -> dict:
         """Load split indices from a file."""
