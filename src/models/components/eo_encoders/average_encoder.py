25837b5ec31304bc099b5f85785750e6a3eb9124
Thijs van der Plas <vdplasthijs@gmail.com>
2026-01-27 14:40:24 +0100

    Reduce asserts

--- src/models/components/eo_encoders/average_encoder.py
+++ src/models/components/eo_encoders/average_encoder.py
@@ -26,16 +26,16 @@ class AverageEncoder(BaseEOEncoder):
         if output_dim is None or output_dim == dict_n_bands_default[eo_data_name]:
             self.output_dim = dict_n_bands_default[eo_data_name]
             self.extra_projector = None
-            self.eo_encoder = self.average
+            self.eo_encoder = self._average
         else:
             assert (
                 type(output_dim) is int and output_dim > 0
             ), f"output_dim must be positive int, got {output_dim}"
             self.output_dim = output_dim
             self.extra_projector = nn.Linear(dict_n_bands_default[eo_data_name], output_dim)
-            self.eo_encoder = self.average_and_project
+            self.eo_encoder = self._average_and_project
 
-    def average(self, x: torch.Tensor) -> torch.Tensor:
+    def _average(self, x: torch.Tensor) -> torch.Tensor:
         """Averages the input tensor over spatial dimensions.
 
         :param x: input tensor of shape (B, C, H, W)
@@ -43,13 +43,12 @@ class AverageEncoder(BaseEOEncoder):
         """
         return x.mean(dim=(-2, -1))
 
-    def average_and_project(self, x: torch.Tensor) -> torch.Tensor:
+    def _average_and_project(self, x: torch.Tensor) -> torch.Tensor:
         """Averages the input tensor over spatial dimensions and projects to output_dim.
 
         :param x: input tensor of shape (B, C, H, W)
         :return: projected tensor of shape (B, output_dim)
         """
-        assert self.extra_projector is not None, "extra_projector is None, cannot project"
         x_avg = x.mean(dim=(-2, -1))
         x_proj = self.extra_projector(x_avg)
         return x_proj
@@ -57,12 +56,11 @@ class AverageEncoder(BaseEOEncoder):
     @override
     def forward(self, batch: Dict[str, torch.Tensor]) -> torch.Tensor:
         eo_data = batch.get("eo", {})
-        assert self.eo_data_name in eo_data, f"eo['{self.eo_data_name}'] not found in batch"
         feats = self.eo_encoder(eo_data[self.eo_data_name])
-        n_nans = torch.sum(torch.isnan(feats)).item()
-        assert (
-            n_nans == 0
-        ), f"AverageEncoder output contains {n_nans}/{feats.numel()} NaNs PRIOR to normalization with data min {eo_data[self.eo_data_name].min()} and max {eo_data[self.eo_data_name].max()}."
+        # n_nans = torch.sum(torch.isnan(feats)).item()
+        # assert (
+        #     n_nans == 0
+        # ), f"AverageEncoder output contains {n_nans}/{feats.numel()} NaNs PRIOR to normalization with data min {eo_data[self.eo_data_name].min()} and max {eo_data[self.eo_data_name].max()}."
         if self.output_normalization == "l2":
             feats = F.normalize(feats, p=2, dim=1)  # L2 normalization (per feature vector)
         elif self.output_normalization == "none":
