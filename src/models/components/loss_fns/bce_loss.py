96ab9bb877efc7e36e1746ddfa76956323617037
gabriele <gabriele.tijunaityte@gmail.com>
2026-02-24 15:19:46 +0100

    Adapt metrics, losses for metrics wrapper

--- src/models/components/loss_fns/bce_loss.py
+++ src/models/components/loss_fns/bce_loss.py
@@ -1,4 +1,4 @@
-from typing import override
+from typing import Dict, override
 
 import torch
 from torch import nn
@@ -10,10 +10,24 @@ class BCELoss(BaseLossFn):
     def __init__(self) -> None:
         super().__init__()
         self.criterion = nn.BCELoss(reduction="mean")
+        self.name: str = "bce_loss"
 
     @override
-    def forward(self, pred: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
-        return self.criterion(pred, labels)
+    def forward(
+        self,
+        pred: torch.Tensor,
+        labels: torch.Tensor | None = None,
+        batch: Dict[str, torch.Tensor] | None = None,
+        **kwargs,
+    ) -> torch.Tensor or Dict[str, torch.Tensor]:
+
+        labels = labels if labels is not None else batch.get("target")
+        loss = self.criterion(pred, labels)
+
+        if "return_label" in kwargs:
+            return {self.name: loss}
+        else:
+            return loss
 
 
 if __name__ == "__main__":
