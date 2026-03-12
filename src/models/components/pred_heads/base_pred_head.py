aea39af74fc30ebe4128fb1a3799e7e7ff45250a
GabrieleTi <gabriele.tijunaityte@gmail.com>
2026-03-12 10:10:26 +0100

    Add setup to prediction heads + docs

--- src/models/components/pred_heads/base_pred_head.py
+++ src/models/components/pred_heads/base_pred_head.py
@@ -7,6 +7,7 @@ from torch import nn
 
 class BasePredictionHead(nn.Module, ABC):
     def __init__(self) -> None:
+        """Base prediction head interface class."""
         super().__init__()
         self.net: nn.Module | None = None
         self.input_dim: int | None = None
@@ -14,16 +15,30 @@ class BasePredictionHead(nn.Module, ABC):
 
     @abstractmethod
     def forward(self, feats: torch.Tensor) -> torch.Tensor:
+        """Forward pass through the prediction head."""
         pass
 
     @final
     def set_dim(self, input_dim: int, output_dim: int) -> None:
+        """Set dimensions for the prediction head configuration.
+
+        :param input_dim: input dimension
+        :param output_dim: output dimension
+        """
+        assert isinstance(self.input_dim, int), TypeError(
+            "Input dimension must be specified as integer"
+        )
+        assert isinstance(self.output_dim, int), TypeError(
+            "Output dimension must be specified as integer"
+        )
         self.input_dim = input_dim
         self.output_dim = output_dim
-        assert type(self.input_dim) is int, self.input_dim
-        if output_dim is not None:
-            assert type(self.output_dim) is int, self.output_dim
 
     @abstractmethod
-    def configure_nn(self) -> None:
+    def setup(self) -> None:
+        """Configures networks, data-dependent parts.
+
+        Gets called in model.setup() method. Returns names of any new module configured to be added
+        to the trainable modules list.
+        """
         pass
