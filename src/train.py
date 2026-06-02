547140a0abda64c92bba5da0bbaf3fdad825773d
GabrieleTi <gabriele.tijunaityte@gmail.com>
2026-06-02 15:23:28 +0200

    Fix logging

--- src/train.py
+++ src/train.py
@@ -151,7 +151,7 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
         )
 
         val_metrics = trainer.callback_metrics
-        wandb_logger.log({f"best_val_{k}": v for k, v in val_metrics.items()})
+        wandb_logger.log_metrics({f"best_val_{k}": v for k, v in val_metrics.items()})
 
     if cfg.get("test"):
         log.info("Starting testing!")
