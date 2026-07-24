0f8a0107635474a909f9c4daf532006b2e9476eb
GabrieleTi <gabriele.tijunaityte@gmail.com>
2026-07-24 13:58:06 +0200

    Remote clip model inplementation

---
+++ src/models/remote_clip_baseline.py
@@ -0,0 +1,51 @@
+import logging
+import os
+
+import open_clip
+import torch
+from huggingface_hub import hf_hub_download
+
+from models.components.geo_encoders.remote_clip_img_encoder import RemoteClipImgEncoder
+from models.components.text_encoders.remote_clip_text_encoder import (
+    RemoteCLIPTextEncoder,
+)
+from models.text_alignment_model import TextAlignmentModel
+
+log = logging.getLogger(__name__)
+
+
+def build_RemoteCLIP_model(
+    model_name: str = "ViT-L-14",
+    hf_cache_dir: str = "../.cache",
+    **kwargs,
+):
+    """Implements RemoteCLIP model as TextAlignmentModel."""
+
+    models = {"RN50": 1024, "ViT-B-32": 512, "ViT-L-14": 768}
+
+    assert model_name in models.keys()
+
+    checkpoint_path = hf_hub_download(
+        "chendelong/RemoteCLIP", f"RemoteCLIP-{model_name}.pt", cache_dir=f"{hf_cache_dir}"
+    )
+
+    model, _, preprocess = open_clip.create_model_and_transforms(model_name)
+    tokenizer = open_clip.get_tokenizer(model_name)
+
+    ckpt = torch.load(
+        f"{os.path.dirname(checkpoint_path)}/RemoteCLIP-{model_name}.pt", map_location="cpu"
+    )
+    log.info(model.load_state_dict(ckpt))
+
+    out_dim = models[model_name]
+
+    geo_encoder = RemoteClipImgEncoder(geo_encoder=model.visual, out_dim=out_dim)
+    model.visual = None
+
+    text_encoder = RemoteCLIPTextEncoder(model=model, tokenizer=tokenizer, out_dim=out_dim)
+
+    return TextAlignmentModel(
+        geo_encoder=geo_encoder,
+        text_encoder=text_encoder,
+        **kwargs,
+    )
