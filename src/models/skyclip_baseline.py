20840c26a73556abeee7d0f5e6ea9c0cd620c8f3
GabrieleTi <gabriele.tijunaityte@gmail.com>
2026-09-18 10:17:08 +0200

    Add div_10000 scaling

--- src/models/skyclip_baseline.py
+++ src/models/skyclip_baseline.py
@@ -114,7 +114,8 @@ def build_SkyCLIP_model(
         assert preprocessing in [
             "div_2000",
             "stretch_2_98",
-        ], "S2 must be preprocessed with preprocessing set to 'div_2000' or 'stretch_2_98'"
+            "div_10000",
+        ], "S2 must be preprocessed with preprocessing set to 'div_2000' or 'stretch_2_98' or 'div_10000'"
         geo_encoder = SkyClipImgEncoder(geo_encoder=model.visual, out_dim=out_dim)
     if return_text_encoder:
         model.visual = None
