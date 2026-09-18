16a5778c974bfbe3397a67001d66d37f4b108c94
GabrieleTi <gabriele.tijunaityte@gmail.com>
2026-09-18 10:18:46 +0200

    Add div_10000 scaling

--- src/models/dofa_clip_baseline.py
+++ src/models/dofa_clip_baseline.py
@@ -41,7 +41,7 @@ def build_DOFACLIP_model(
             "div_2000",
             "div_10000",
             "stretch_2_98",
-        ], "S2 must be preprocessed with preprocessing set to 'div_2000'"
+        ], "S2 must be preprocessed with preprocessing set to 'div_2000' or 'div_10000' or 'stretch_2_98'"
         geo_encoder = DOFAClipImgEncoder(
             geo_encoder=model.visual, out_dim=out_dim, geo_data_name=geo_data_name
         )
